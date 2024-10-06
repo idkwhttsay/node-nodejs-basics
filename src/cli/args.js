5409147efa019837eb041ebbb14ed4d9df6382de
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-06 16:51:46 -0400

    feat: cli completed

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+
+  const filteredArgs = args
+    .filter((name, index) => index % 2 === 0)
+    .map((name, index) => `${name.slice(2)} is ${args[index * 2 + 1]}`)
+    .join(", ");
+
+  console.log(filteredArgs);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const filteredEnv = Object.keys(process.env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join("; ");
+
+  console.log(filteredEnv);
 };
 
 parseEnv();
