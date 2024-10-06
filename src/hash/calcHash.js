7c507a286a98b8952239cf9774a8eecbe44dd7c3
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-06 17:55:06 -0400

    feat: hash completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const pathToHashFile = path.join(
+      __dirname,
+      "files",
+      "fileToCalculateHashFor.txt",
+    );
+
+    const hash = crypto.createHash("sha256");
+    const readStream = fs.createReadStream(pathToHashFile);
+
+    readStream.pipe(hash).on("finish", () => {
+      console.log(`SHA256 of a file: ${hash.digest("hex")}`);
+    });
+  } catch (error) {
+    console.error(`Error occurred while calculating hash: ${error.message}`);
+  }
 };
 
 await calculateHash();
