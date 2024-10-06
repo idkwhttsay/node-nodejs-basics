cc92ac7a401374067c3f471abeacaec1e71a392a
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-06 19:35:43 -0400

    feat: wt, cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { fork } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToScriptFile = path.join(__dirname, "files", "script.js");
+
+  const cp = fork(pathToScriptFile, args);
+
+  cp.on("message", (message) => {
+    console.log(`Received from child process: ${message}`);
+  });
+
+  cp.on("close", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1, arg2, arg3"]);
